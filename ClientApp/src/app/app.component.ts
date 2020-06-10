4b6511110bbdfb5994b9ea17c74461c7fa5b3791
berliners1 <steven@stevenberliner.me>
2020-06-10 14:47:03 -0500

    Combine all json objects into an array asynchronously within a for await of loop

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -9,8 +9,8 @@ import { VideosFromChannel } from './models/VideosFromChannel';
 })
 export class AppComponent {
   title = 'app';
-  videoTitle: string[] = new Array();
-  channelTitle: string;
+  channelVideosDetails: any[] = new Array();
+  combinedChannelsArray: any[] = new Array();
 
   ROOT_URL: string = 'https://localhost:44399/api/youtube/';
   CHANNEL_ID: string;
@@ -20,19 +20,43 @@ export class AppComponent {
 
   constructor(public auth: AuthService, private http: HttpClient){}
 
-  getPosts(addedChannelIds){
+  
+  getPosts = async(addedChannelIds) => {
 
-    for(let i = 0; i < addedChannelIds.length; i++){
-      this.FULL_SEARCH_URL = this.ROOT_URL + "bychannelid/" + addedChannelIds[i];
-      
+    let i = 0;
+    for await(let addedChannelId of addedChannelIds){
+      this.FULL_SEARCH_URL = this.ROOT_URL + "bychannelid/" + addedChannelId;
+
       this.http.get<VideosFromChannel>(this.FULL_SEARCH_URL)
       .subscribe(data => {
-        this.videoTitle[i] = data.items[i].snippet.title;
-        console.log("video " + this.videoTitle[i]);
-      });
-    }
 
+        //Have all "data's" existing at the same time in the same (final) iteration.
+        this.channelVideosDetails[i] = data;
+
+        //Combine all the separate 'channel' arrays into one big array that contains recent videos from all channels.
+        this.combinedChannelsArray.push(this.channelVideosDetails[i]);
+        
+        //Make sure the loop is on its final iteration before doing this.
+        if(i === addedChannelIds.length - 1){
+          //console.log(this.combinedChannelsArray[0].items[0].snippet.title);
+          console.log(this.combinedChannelsArray);
+
+          for(let j = 0; j < data.items.length; j++){
+            console.log(this.combinedChannelsArray[0].items[j].snippet.title);
+          }
+          for(let j = 0; j < data.items.length; j++){
+            console.log(this.combinedChannelsArray[1].items[j].snippet.title);
+          }
+        }
+
+        i++;
+        
+      });
+      
+      
+    }
   }
+
 }
 
 
