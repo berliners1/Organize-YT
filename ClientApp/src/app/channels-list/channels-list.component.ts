5675a022a45712e690570d2ad3fc3c4b85c05b1a
berliners1 <steven@stevenberliner.me>
2020-06-12 22:37:47 -0500

    Refresh all displayed items every time getPosts runs, so there are no old leftovers when new data renders from firebase

--- ClientApp/src/app/channels-list/channels-list.component.ts
+++ ClientApp/src/app/channels-list/channels-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, SimpleChanges} from '@angular/core';
+import { Component, Input, SimpleChanges, Injectable} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { VideosFromChannel } from '../models/VideosFromChannel';
 import { AngularFirestore } from '@angular/fire/firestore';
@@ -24,54 +24,52 @@ export class ChannelsListComponent {
   @Input() user: any;
 
   ngOnChanges(changes: SimpleChanges){
+
     if(this.userAddedChannelIds){
+      console.log('get posts');
+      console.log("onChanges- " + this.userAddedChannelIds);
       this.getPosts(this.userAddedChannelIds);
+    } else {
+      console.log('no posts');
     }
   }
   
   getPosts = async(addedChannelIds) => {
-    if(this.canGo){
-      this.canGo = false;
+    let i = 0;
+    for await(let addedChannelId of addedChannelIds){
 
-      let i = 0;
+      this.FULL_SEARCH_URL = this.ROOT_URL + "bychannelid/" + addedChannelId;
 
-      for await(let addedChannelId of addedChannelIds){
+      this.http.get<VideosFromChannel>(this.FULL_SEARCH_URL)
+      .subscribe(data => {
+        //Have all "data's" existing at the same time in the same (final) iteration.
+        this.channelVideosDetails[i] = data;
+
+        console.log("subscribe-");
+        console.log(this.channelVideosDetails[i]);
+
+        //Combine all the separate 'channel' arrays into one big array that contains recent videos from all channels.
+        this.combinedChannelsArray.push(this.channelVideosDetails[i]);
         
-        this.FULL_SEARCH_URL = this.ROOT_URL + "bychannelid/" + addedChannelId;
+        i++;
+      });
+    }
 
-        this.http.get<VideosFromChannel>(this.FULL_SEARCH_URL)
-        .subscribe(data => {
-
-          //Have all "data's" existing at the same time in the same (final) iteration.
-          this.channelVideosDetails[i] = data;
-
-          //Combine all the separate 'channel' arrays into one big array that contains recent videos from all channels.
-          this.combinedChannelsArray.push(this.channelVideosDetails[i]);
-
-          i++;
-        });
-      }
-
-      setTimeout(function(){
-        this.canGo = true;
-      }, 1000)
-
-    } else {
-      console.log("too soon to call api again");
+    //display:none existing channels and replace with what is now current in firebase's db, to avoid duplicates.
+    let channelsToRemove = document.querySelectorAll('.channel-class');
+    for(let j = 0; j < channelsToRemove.length; j++){
+      channelsToRemove[j].classList.add('hidden');
     }
   }
 
   deleteChannel(channel, user){
     //replace UC with UU at the start of channel ID
-    let channelId = channel.items[0].snippet.channelId.replace(/^.{2}/g, "UU");
+    let channelIdToRemove = channel.items[0].snippet.channelId.replace(/^.{2}/g, "UU");
     
     //remove specified UU-id from the addedChannelIds array
     this.afs.doc(`users/${user.uid}`).update({
-      addedChannelIds: firebase.firestore.FieldValue.arrayRemove(channelId)
+      addedChannelIds: firebase.firestore.FieldValue.arrayRemove(channelIdToRemove)
     });
-
-    //To do: run getPosts again to refresh current list of channels. Also on search.component.ts.
-    
   }
 
 }
